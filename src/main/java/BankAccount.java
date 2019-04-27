d72c1e5aa7a721de79c9c78e116525708bd2e183
Florin Langer <florin@berkeley.edu>
2019-04-27 14:17:43 -0700

    starting now

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,9 @@
+// understands balance of a user
+public class BankAccount {
+
+    int balance;
+    
+    BankAccount(int initialDeposit) {
+        this.balance = initialDeposit;
+    }
+}
---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BankAccountTest {
+
+    @Test
+    public void canCreateBankAccount() {
+        BankAccount a = new BankAccount(0);
+//        assertTrue(a.deposit(10));
+    }
+}
