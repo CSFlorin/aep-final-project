b8707920cde03befa5aa8470760f0167453b2a38
Florin Langer <florin@berkeley.edu>
2019-04-27 15:45:52 -0700

    test 8

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -4,75 +4,68 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    int jezPin = 1234;
+    User jez = new User("jez", jezPin);
+
     @Test
     public void canCreateBankAccount() {
-        int pin = 1234;
-        User jez = new User("jez", pin);
         BankAccount a = new BankAccount(0f, jez);
     }
 
     @Test
     public void canDeposit() {
-        int pin = 1234;
-        User jez = new User("jez", pin);
         BankAccount a = new BankAccount(0f, jez);
         assertTrue(a.deposit(10f));
-        assertEquals(10f, a.getBalance(pin), 0f);
+        assertEquals(10f, a.getBalance(jezPin), 0f);
     }
 
     @Test
     public void canWithdraw() {
-        int pin = 1234;
-        User jez = new User("jez", pin);
         BankAccount a = new BankAccount(100f, jez);
-        assertTrue(a.withdraw(10f, pin));
-        assertEquals(90f, a.getBalance(pin), 0f);
+        assertTrue(a.withdraw(10f, jezPin));
+        assertEquals(90f, a.getBalance(jezPin), 0f);
     }
 
     @Test
     public void cannotOverdraw() {
-        int pin = 1234;
-        User jez = new User("jez", pin);
         BankAccount a = new BankAccount(100f, jez);
-        assertFalse(a.withdraw(101f, pin));
-        assertEquals(100f, a.getBalance(pin), 0f);
+        assertFalse(a.withdraw(101f, jezPin));
+        assertEquals(100f, a.getBalance(jezPin), 0f);
     }
 
     @Test
     public void canTransfer() {
-        int pin = 1234;
-        User jez = new User("jez", pin);
         BankAccount a = new BankAccount(100f, jez);
         BankAccount b = new BankAccount(100f, jez);
-        assertTrue(a.transfer(b, 50f, pin));
-        assertEquals(50f, a.getBalance(pin), 0f);
-        assertEquals(150f, b.getBalance(pin), 0f);
+        assertTrue(a.transfer(b, 50f, jezPin));
+        assertEquals(50f, a.getBalance(jezPin), 0f);
+        assertEquals(150f, b.getBalance(jezPin), 0f);
     }
 
     @Test
     public void cannotTransferMoreThanBalance() {
-        int pin = 1234;
-        User jez = new User("jez", pin);
         BankAccount a = new BankAccount(100f, jez);
         BankAccount b = new BankAccount(100f, jez);
-        assertFalse(a.transfer(b, 101f, pin));
-        assertEquals(100f, a.getBalance(pin), 0f);
-        assertEquals(100f, b.getBalance(pin), 0f);
+        assertFalse(a.transfer(b, 101f, jezPin));
+        assertEquals(100f, a.getBalance(jezPin), 0f);
+        assertEquals(100f, b.getBalance(jezPin), 0f);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void cannotDepositNegativeAmount() {
-        int pin = 1234;
-        User jez = new User("jez", pin);
         BankAccount a = new BankAccount(-100f, jez);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void cannotWithdrawNegativeAmount() {
-        int pin = 1234;
-        User jez = new User("jez", pin);
         BankAccount a = new BankAccount(0f, jez);
-        a.withdraw(-100f, pin);
+        a.withdraw(-100f, jezPin);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void cannotGetBalanceWithInvalidPin() {
+        BankAccount a = new BankAccount(100f, jez);
+        a.getBalance(1235);
     }
 
 //    @Test
